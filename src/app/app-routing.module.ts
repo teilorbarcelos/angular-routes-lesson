80314f56a081cac22849cc2faf40e2bc83268186
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-09-11 15:19:35 -0300

    children routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,11 @@ import { NotFoundComponent } from './pages/not-found/not-found.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'about/:id/:username', component: AboutComponent },
+  {
+    path: 'about',
+    component: AboutComponent,
+    children: [{ path: ':id/:username', component: AboutComponent }],
+  },
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '404' },
 ]
